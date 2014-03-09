4bfb6f7a5478f31d0b1681b4af1ac6b7e9b9728b
Albert Wu <albert12132@gmail.com>
2014-03-09 00:48:30 -0800

    Menu buttons

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 $(function() {
   outputBoard(generateTestBoard());
-  $('confirm-move').click(chooseMoveButton);
+  $('#button-move-menu').click(function() {
+    $('#main-menu').fadeOut(function() {
+      $('#move-menu').fadeIn();
+    });
+  });
+  $('#button-item-menu').click(function() {
+    $('#main-menu').fadeOut(function() {
+      $('#item-menu').fadeIn();
+    });
+  });
+  $('#move-menu-back-button').click(function() {
+    $('#move-menu').fadeOut(function() {
+      $('#main-menu').fadeIn();
+    });
+  });
+  $('#item-menu-back-button').click(function() {
+    $('#item-menu').fadeOut(function() {
+      $('#main-menu').fadeIn();
+    });
+  });
+  $('#fund-menu-back-button').click(function() {
+    $('#fund-menu').fadeOut(function() {
+      $('#item-menu').fadeIn();
+    });
+  });
+  $('.item-button').click(function() {
+    $('#fund-menu-item').text($(this).text());
+    $('#item-menu').fadeOut(function() {
+      $('#fund-menu').fadeIn();
+    });
+  });
 });
