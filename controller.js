831e311c5a0f98eca8e9582a8bbaf7813384bff7
Derric Chien <derric@Derrics-Computer.local>
2014-03-09 03:36:18 -0700

    made changes

--- controller.js
+++ controller.js
@@ -166,9 +166,8 @@ function selectFundersButton() {
 
 */
 function fundButton() {
-	//var totalFunds = $('player0fund').val() + $('player1fund').val() + $('player2fund').val() + $('player3fund').val();
-	//console.log(totalFunds);
-	return 5;
+	var totalFunds = $('player0fund').val() + $('player1fund').val() + $('player2fund').val(); //+ $('player3fund').val();
+	console.log(totalFunds);
 	//if (totalFunds < item.price) {
 		//alert("Not enough funds!");
 	//} else {
